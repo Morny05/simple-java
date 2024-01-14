287c5c699b082cebee47a8624577b27978e31d36
Bouda Azerty <mohamedmorny01@gmail.com>
2024-01-14 10:58:09 +0100

    simple JAVA

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Never walk alone!");
+        mutliply(1313,1);
+        mutliply(22, 10);
+        mutliply(2, 5);
+    }
+    public static void welcome() {
+    }
+    public static void mutliply(int a, int b) {
+        System.out.println(a * b);
+    }
+}
